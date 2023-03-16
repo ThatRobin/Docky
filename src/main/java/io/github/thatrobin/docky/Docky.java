917c692e41e6dd63ed9add83a2eb102d814d1f2a
Robin <ThatRobin3001@gmail.com>
2023-03-16 12:21:58 +0000

    removed entrypoint for Docky.java

--- src/main/java/io/github/thatrobin/docky/Docky.java
+++ src/main/java/io/github/thatrobin/docky/Docky.java
@@ -1,16 +1,10 @@
 package io.github.thatrobin.docky;
 
-import io.github.thatrobin.docky.utils.DataTypeLoader;
-import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class Docky implements ModInitializer {
+public class Docky {
 
     public static final Logger LOGGER = LogManager.getLogger(Docky.class);
 
-    @Override
-    public void onInitialize() {
-        DataTypeLoader.load();
-    }
 }
